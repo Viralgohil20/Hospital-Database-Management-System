f1ab8d3023830864dc4f1794620af7a14ce0f3c9
Viral Gohil <124395211+Viralgohil20@users.noreply.github.com>
2024-02-27 12:10:55 -0500

    Add files via upload

---
+++ HDBMS.sql
@@ -0,0 +1,258 @@
+USE HospitalManagement_Group11;
+
+SELECT * FROM Hospital;
+SELECT * FROM Doctor;
+SELECT * FROM Patient;
+insert into Patient values(1, 'David','W','Warner','davidwarner@gmail.com', 2346547890, '06-05-1988', 'M', '07-30-2022', '75 Saint Street', 'Boston','MA', 22102);
+insert into Patient values(2, 'Virat','A','Kohli','virat.kohli@gmail.com', 3364457420, '06-07-1998', 'M', '07-30-2021', '21 Smith Street', 'NewYork','NY', 22120);
+insert into Patient values(3, 'Rohit','S','Sharma','sharma.rohit@gmail.com', 6265988004, '10-16-1965', 'M', '12-30-2021', 'Fenway Park Street', 'Los Angeles','CA', 99950);
+insert into Patient values(4, 'Rishab','M','Pant','pant.rish@gmail.com', 3697516998, '04-09-1984', 'M', '01-12-2022', '370 Huntington Street', 'San Diego','CA', 85001);
+insert into Patient values(5, 'Mayank','R','Gandhi','mayank.g@gmail.com', 6265988004, '05-13-2005', 'M', '05-20-2022', '75 Tremont Street', 'San Francisco','CA', 12112);
+insert into Patient values(6, 'Niyati','M','Shah','shah.niyati@gmail.com', 7804055277, '09-27-2007', 'F', '05-20-2022', '21A Highland Street', 'San Diego','CA', 21002);
+insert into Patient values(7, 'Moksha','A','Doshi','doshi.mok@gmail.com', 8451031651, '11-04-2002', 'F', '07-30-2022', '2C Smith Street', 'Austin','TX', 40002);
+insert into Patient values(8, 'Krushna','A','Tyagi','ktyagi@gmail.com', 5877839071, '03-20-1999', 'F', '06-04-2022', ' Roxbury Crossing Park Street', 'Nashville','TN', 40001);
+insert into Patient values(9, 'Sanay','S','Shah','shahsanay@gmail.com', 7804056507, '04-12-1996', 'M', '06-20-2022', 'Parker Street Near Lechmere', 'San Jose','CA', 99950);
+insert into Patient values(10, 'Albert','W','Pruto','pruto.albert@gmail.com', 7804053849, '04-25-1997', 'M', '03-23-2022', '12 Downtown Jamaican Plain', 'San Antonio','TX', 35004);
+insert into Patient values(11, 'David','P','Paul','davidpaul@gmail.com', 1346547878, '01-02-1986', 'M', '04-10-2022', '85 Saint Street', 'Boston','MA', 22102);
+insert into Patient values(12, 'Viraj','M','Kali','virat.kali@gmail.com', 3364457420, '06-07-1998', 'M', '04-30-2021', '21 Smith Street', 'NewYork','NY', 22120);
+insert into Patient values(13, 'Rajat','S','Sharma','sharma.rajat@gmail.com', 6265988004, '10-16-1965', 'M', '01-30-2021', 'Fenway Park Street', 'Los Angeles','CA', 99950);
+insert into Patient values(14, 'Roashan','M','Subhash','subh.rosh@gmail.com', 3697516998, '04-09-1984', 'M', '01-12-2022', '370 Huntington Street', 'San Diego','CA', 85001);
+insert into Patient values(15, 'Maya','R','Gandhi','maya.g@gmail.com', 6265988004, '05-13-2005', 'M', '01-20-2022', '75 Tremont Street', 'San Francisco','CA', 12112);
+insert into Patient values(16, 'Niya','M','Shah','shah.niya@gmail.com', 7804055277, '09-27-2007', 'F', '01-25-2022', '21A Highland Street', 'San Diego','CA', 21002);
+insert into Patient values(17, 'Mohan','A','Das','das.mohan@gmail.com', 8451031651, '11-04-2002', 'F', '01-10-2022', '2C Smith Street', 'Austin','TX', 40002);
+insert into Patient values(18, 'Krish','A','Kapoor','kkrish@gmail.com', 5877839071, '03-20-1999', 'F', '01-04-2022', ' Roxbury Crossing Park Street', 'Nashville','TN', 40001);
+insert into Patient values(19, 'Sanay','S','Patel','patelsanay@gmail.com', 7804056507, '04-12-1996', 'M', '01-20-2022', 'Parker Street Near Lechmere', 'San Jose','CA', 99950);
+insert into Patient values(20, 'Alam','W','Rito','rito.alam@gmail.com', 7804053849, '04-25-1997', 'M', '01-23-2022', '12 Downtown Jamaican Plain', 'San Antonio','TX', 35004);
+insert into Patient values(21, 'Sam','G','Pat','patsam@gmail.com', 2346547890, '06-05-1988', 'M', '01-10-2022', '75 Saint Street', 'Boston','MA', 22102);
+insert into Patient values(22, 'Vikas','A','Purohit','vikas.purohit@gmail.com', 3364457420, '06-07-1998', 'M', '01-30-2021', '21 Smith Street', 'NewYork','NY', 22120);
+insert into Patient values(23, 'Neha','D','Dongre','neha.d@gmail.com', 6265988004, '10-16-1996', 'F', '11-30-2021', 'Fenway Park Street', 'Los Angeles','CA', 99950);
+insert into Patient values(24, 'Rish','M','Dave','dave.rish@gmail.com', 3697516998, '04-09-1984', 'M', '01-12-2022', '370 Huntington Street', 'San Diego','CA', 85001);
+insert into Patient values(25, 'Sagar','P','Gandhi','sagar.g@gmail.com', 6265988004, '05-13-2005', 'M', '04-20-2022', '75 Tremont Street', 'San Francisco','CA', 12112);
+insert into Patient values(26, 'Niyam','M','Shah','shah.niyam@gmail.com', 7804055277, '09-27-2007', 'M', '04-20-2022', '21A Highland Street', 'San Diego','CA', 21002);
+insert into Patient values(27, 'Moksh','A','Doshi','doshi.mok@gmail.com', 8451031651, '11-04-2002', 'M', '04-30-2022', '2C Smith Street', 'Austin','TX', 40002);
+insert into Patient values(28, 'Kajal','A','Tyagi','ktyagi@gmail.com', 5877839071, '03-20-1999', 'F', '01-04-2022', ' Roxbury Crossing Park Street', 'Nashville','TN', 40001);
+insert into Patient values(29, 'Manay','S','Shah','shahmanay@gmail.com', 7804056507, '04-12-1996', 'M', '04-20-2022', 'Parker Street Near Lechmere', 'San Jose','CA', 99950);
+insert into Patient values(30, 'Adam','W','Pauja','pauja.adam@gmail.com', 7804053849, '04-25-1997', 'M', '03-23-2022', '12 Downtown Jamaican Plain', 'San Antonio','TX', 35004);
+insert into Patient values(31, 'Benjamin','T','Franklin','frankbenj@gmail.com', 2346547890, '06-05-1988', 'M', '04-30-2022', '75 Saint Street', 'Boston','MA', 22102);
+insert into Patient values(32, 'Viral','H','Gohil','viral.gohil@gmail.com', 3364457420, '06-07-1998', 'M', '03-30-2021', '21 Smith Street', 'NewYork','NY', 22120);
+insert into Patient values(33, 'Roshni','S','Sharma','sharma.rosh@gmail.com', 6265988004, '10-16-1995', 'F', '12-30-2021', 'Fenway Park Street', 'Los Angeles','CA', 99950);
+insert into Patient values(34, 'Rishi','M','Gavkar','g.rishi@gmail.com', 3697516998, '04-09-1984', 'M', '01-12-2022', '370 Huntington Street', 'San Diego','CA', 85001);
+insert into Patient values(35, 'Mayur','R','Pandey','mayur.p@gmail.com', 6265988004, '05-13-2005', 'M', '01-20-2022', '75 Tremont Street', 'San Francisco','CA', 12112);
+insert into Patient values(36, 'Namo','M','Pandey','pandey.namo@gmail.com', 7804055277, '09-27-2007', 'F', '01-20-2022', '21A Highland Street', 'San Diego','CA', 21002);
+insert into Patient values(37, 'Nita','A','Doshi','doshi.nita@gmail.com', 8451031651, '11-04-1968', 'F', '01-30-2022', '2C Smith Street', 'Austin','TX', 40002);
+insert into Patient values(38, 'Rajvi','S','Porwal','prajvi@gmail.com', 5877839071, '03-20-1995', 'F', '01-04-2022', ' Roxbury Crossing Park Street', 'Nashville','TN', 40001);
+insert into Patient values(39, 'Het','S','Shah','shahhet@gmail.com', 7804056507, '11-04-2004', 'M', '01-20-2022', 'Parker Street Near Lechmere', 'San Jose','CA', 99950);
+insert into Patient values(40, 'Ajay','A','Doshi','doshi.ajay@gmail.com', 7804053849, '04-25-1967', 'M', '01-23-2022', '12 Downtown Jamaican Plain', 'San Antonio','TX', 35004);
+insert into Patient values(41, 'Barney','T','Stinson','sbarney@gmail.com', 2346547890, '06-05-1968', 'M', '04-30-2022', '75 Saint Street', 'Boston','MA', 22102);
+insert into Patient values(42, 'Marshal','H','Ericksen','marshal.eric@gmail.com', 3364457420, '06-07-1998', 'M', '03-30-2021', '21 Smith Street', 'NewYork','NY', 22120);
+insert into Patient values(43, 'Robin','S','Schrebatsky','srobin@gmail.com', 6265988004, '10-16-1995', 'F', '12-30-2021', 'Fenway Park Street', 'Los Angeles','CA', 99950);
+insert into Patient values(44, 'Ted','M','Mosby','m.ted@gmail.com', 3697516998, '04-09-1984', 'M', '01-12-2022', '370 Huntington Street', 'San Diego','CA', 85001);
+insert into Patient values(45, 'Lily','M','Ericksen','e.lily@gmail.com', 6265988004, '05-13-2005', 'M', '01-20-2022', '75 Tremont Street', 'San Francisco','CA', 12112);
+insert into Patient values(46, 'Namo','M','Guhad','pandey.namo@gmail.com', 7804055277, '09-27-2007', 'F', '01-20-2022', '21A Highland Street', 'San Diego','CA', 21002);
+insert into Patient values(47, 'Sara','S','Khan','khansara@gmail.com', 8451031651, '11-04-1992', 'F', '01-30-2022', '2C Smith Street', 'Austin','TX', 40002);
+insert into Patient values(48, 'Samuel','S','Adam','adamsam@gmail.com', 5877839071, '03-20-1995', 'F', '01-04-2022', ' Roxbury Crossing Park Street', 'Nashville','TN', 40001);
+insert into Patient values(49, 'Khilji','S','Shah','shahkhilji@gmail.com', 7804056507, '11-04-2004', 'M', '01-20-2022', 'Parker Street Near Lechmere', 'San Jose','CA', 99950);
+insert into Patient values(50, 'Asma','A','Kyle','kyleasma@gmail.com', 4804053849, '04-21-1987', 'F', '01-23-2022', '2 Downtown Jamaican Plain', 'San Antonio','TX', 35004);
+
+
+UPDATE dbo.Patient
+   SET Zipcode = 40001
+WHERE idPatient = 8
+
+
+
+SELECT * FROM Test
+SELECT * FROM Types_of_Cases
+insert into Types_of_Cases values('Critical', 'Short of breath', '2022-08-02 15:32:06', NULL, 1, 29, 77, 19)
+insert into Types_of_Cases values('Minor', 'Regular Check up', NULL, NULL, 2, NULL, NULL, NULL)
+insert into Types_of_Cases values('Critical', 'Burn on face, chest, back', '2022-08-02 05:12:06', NULL, 11, 29, 139, 19)
+insert into Types_of_Cases values('Major', 'Covid- Low Oxygen Level', '2022-06-22 15:32:06', '2022-06-30 10:22:06', 4, 2, 27, 114)
+insert into Types_of_Cases values('Critical','Heart Attack Severe pain in chest', '2022-07-29 09:02:46', NULL, 5, 2, 57, 114)
+insert into Types_of_Cases values('Critical','Tumour- Internal Bleeding', '2022-07-28 15:32:06', NULL, 6, 32, 123, 11)
+insert into Types_of_Cases values('Minor','Arthritis - Pain in joints', Null, NULL, 7,NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Stone in stomach', Null, NULL, 8, NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Dizziness', Null, NULL, 9, NULL, NULL, NULL)
+insert into Types_of_Cases values('Major','Pneumonia', '2022-04-04 10:10:10', '2022-04-22 12:10:49', 10, 32, 147, 11)
+
+
+
+insert into Types_of_Cases values('Critical', 'Accident', '2022-08-01 15:32:06', NULL, 22, 9, 97 ,25)
+insert into Types_of_Cases values('Minor', 'Regular Check up', NULL, NULL, 23, NULL, NULL, NULL)
+insert into Types_of_Cases values('Critical', 'Suicide Attempt', '2022-08-02 01:12:06', NULL, 24, 9, 170, 25)
+insert into Types_of_Cases values('Major', 'Covid- Low Oxygen Level', '2022-04-27 15:32:06', '2022-05-13 11:22:06', 25, 27, 18, 28)
+insert into Types_of_Cases values('Critical','Heart Attack Severe pain in chest', '2022-07-24 09:02:46', '2022-08-01 10:00:00', 26, 18, 260, 48)
+insert into Types_of_Cases values('Critical','Tumour- Internal Bleeding', '2022-07-22 15:32:06', '2022-07-31 11:22:00', 27, 41, 33, 143)
+insert into Types_of_Cases values('Minor','Covid', '2022-04-22 15:32:06', '2022-04-28 15:32:06', 28, 41, 67, 143)
+insert into Types_of_Cases values('Minor','Covid', '2022-04-12 15:32:06', '2022-04-20 15:32:06', 29, 20, 163, 131)
+insert into Types_of_Cases values('Minor','Dizziness', '2022-03-05 15:32:06', '2022-03-2 15:32:06', 30, 33, 155, 99)
+insert into Types_of_Cases values('Major','Surgery disk replacement', '2022-04-04 10:10:10', '2022-04-22 12:10:49', 31, 20, 53, 131)
+
+
+DROP TRIGGER Trig_BedAvail
+----------------------
+
+insert into Types_of_Cases values('Critical', 'Short of breath', '2022-06-22 15:32:06', NULL, 27, 14, 34,72)
+insert into Types_of_Cases values('Minor', 'Regular Check up', NULL, NULL, 28, NULL, NULL, NULL)
+insert into Types_of_Cases values('Critical', 'Burn on face, chest, back', '2022-08-02 05:12:06', NULL, 29, 32, 36, 79)
+insert into Types_of_Cases values('Major', 'Covid- Low Oxygen Level', '2022-06-22 15:32:06', '2022-06-30 10:22:06', 30, 12, 116, 139)
+insert into Types_of_Cases values('Critical','Heart Attack Severe pain in chest', '2022-07-29 09:02:46', NULL, 31, 14, 54, 70)
+insert into Types_of_Cases values('Critical','Tumour- Internal Bleeding', '2022-06-22 15:32:06', NULL, 32, 17, 55, 109)
+insert into Types_of_Cases values('Minor','Arthritis - Pain in joints', Null, NULL, 33,NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Stone in stomach', Null, NULL, 34, NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Dizziness', Null, NULL, 35, NULL, NULL, NULL)
+insert into Types_of_Cases values('Major','Pneumonia', '2022-04-04 10:10:10', '2022-04-22 12:10:49', 36, 40, 59, 24)
+
+
+insert into Types_of_Cases values('Critical', 'Short of breath', '2022-06-22 15:32:06', NULL, 37, 1, 7,31)
+insert into Types_of_Cases values('Minor', 'Regular Check up', NULL, NULL, 38, NULL, NULL, NULL)
+insert into Types_of_Cases values('Critical', 'Burn on face, chest, back', '2022-03-05 05:12:06', NULL, 39, 1, 1, 49)
+insert into Types_of_Cases values('Major', 'Covid- Low Oxygen Level', '2022-06-22 15:32:06', '2022-06-30 10:22:06', 40, 7, 6, 8)
+insert into Types_of_Cases values('Critical','Heart Attack Severe pain in chest', '2022-01-24 09:02:46', NULL, 41, 4, 8, 45)
+insert into Types_of_Cases values('Critical','Tumour- Internal Bleeding', '2022-06-22 15:32:06', NULL, 42, 7, 10, 70)
+insert into Types_of_Cases values('Minor','Arthritis - Pain in joints', Null, NULL, 43,NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Stone in stomach', Null, NULL, 44, NULL, NULL, NULL)
+insert into Types_of_Cases values('Minor','Dizziness', Null, NULL, 45, NULL, NULL, NULL)
+insert into Types_of_Cases values('Major','Pneumonia', '2022-04-04 10:10:10', '2022-04-22 12:10:49', 46, 10, 72, 150)
+
+
+SELECT * FROM Bill;
+insert into Bill values(1,4244.45, '09-10-2022', 'Debit Card', 7)
+insert into Bill values(2,1657.985, '02-11-2022', 'Credit Card', 1)
+insert into Bill values(3,343356.2, '01-21-2022', 'Online', 6)
+insert into Bill values(4,420.3, '03-20-2022', 'Credit Card', 5)
+insert into Bill values(5,980.6767, '04-19-2022', 'Cash', 4)
+insert into Bill values(6,12086.876, '05-18-2022', 'Debit Card', 3)
+insert into Bill values(7,22234.90, '10-12-2022', 'Credit Card', 2)
+insert into Bill values(8,1677.1345, '11-09-2022', 'Debit Card', 10)
+insert into Bill values(9,132612.09, '12-31-2022', 'Online', 8)
+insert into Bill values(10,76382.56, '09-14-2022', 'Credit Card', 9)
+
+
+
+
+SELECT * FROM Ward;
+SELECT * FROM Bed_Details;
+SELECT * FROM Room_Details;
+
+SELECT  Hospital_ID, w.idWard, idBed_Details, bd.Room_No
+FROM Ward w JOIN Room_Details rd ON w.idWard = rd.idWard
+JOIN Bed_Details bd ON bd.Room_No= rd.Room_No
+WHERE rd.Availability='Yes' and bd.Availability='Yes'
+ORDER BY Hospital_ID;
+
+SELECT toc.Case_ID, toc.idWard, Hospital_ID
+from Types_of_Cases toc JOIN Ward w ON w.idWard= toc.idWard
+
+
+SELECT * FROM Types_of_Cases --69, 71,
+UPDATE dbo.Types_of_Cases
+   SET [Admit Date] = '2022-07-29 09:02:46'
+WHERE Case_ID = 71
+
+
+
+SELECT * FROM Test
+insert into Test values('CT Scan', '2022-08-02', 234.50, 67, 101)
+insert into Test values('Urinalysis', '2022-08-03', 550.99, 69, 101)
+insert into Test values('CT Scan, X-ray, Blood test', '2022-06-23', 775.45, 70, 201)
+insert into Test values('ECG', '2022-07-29', 370, 71, 201)
+insert into Test values('MRI', '2022-07-30', 450, 72, 301)
+insert into Test values('CT Scan', '2022-04-12', 300, 76, 301)
+insert into Test values('X-ray, CT Scan', '2022-08-01', 680.75, 82, 101)
+insert into Test values('CT Scan', '2022-04-24', 234.50, 88, 401)
+insert into Test values('CT Scan', '2022-04-15', 234.50, 89, 401)
+insert into Test values('CT Scan', '2022-08-02', 234.50, 90, 401)
+
+SELECT * FROM Laboratory
+SELECT * FROM Hospital
+
+SELECT * FROM Bill......caseid-hpid, wdid
+SELECT * FROM Ward
+SELECT * FROM Types_of_Cases
+
+CREATE FUNCTION DayIncome 
+(@Date datetime, @Hospital int)
+RETURNS int
+AS
+BEGIN
+	Declare @TotalIncome int;
+	(SELECT @TotalIncome = SUM(b.Total_Bill)
+	FROM Bill b JOIN Types_of_Cases toc ON b.Case_ID= toc.Case_ID
+
+
+
+END;
+
+
+SELECT  w.Hospital_ID, SUM(b.Total_Bill) as Income
+FROM Bill b LEFT JOIN Types_of_Cases toc ON b.Case_ID= toc.Case_ID JOIN Ward w ON w.idWard = toc.idWard
+GROUP BY w.Hospital_ID
+--HAVING w.Hospital_ID = 4 
+
+
+
+
+
+
+
+
+
+CREATE FUNCTION GetTotalSales
+(@BeginYear int, @EndYear int, @Month int)
+RETURNS int
+AS
+BEGIN
+	Declare @TotalSales int;
+	(SELECT @TotalSales = SUM(TotalDue)
+	FROM [AdventureWorks2008R2].Sales.SalesOrderHeader
+	WHERE CAST(YEAR(OrderDate) AS int) BETWEEN @BeginYear AND @EndYear AND CAST(MONTH(OrderDate) AS int) = @Month)
+	 IF (@TotalSales IS NULL)
+	 SET @TotalSales = 0
+RETURN @TotalSales
+END;
+
+SELECT dbo.GetTotalSales(2002,2008, 09) AS [Total Sales in the given range];
+
+DROP FUNCTION GetTotalSales;
+
+
+SELECT * FROM Types_of_Cases
+WHERE [Discharge Date] is not NULL
+
+
+
+Select w.hospital_id,p.idPatient,p.Patient_FName name,p.Patient_LName ame,p.AGE,t.case_id,w.idward,w.[Floor Number] ,bd.room_no,bd.idbed_details
+
+from patient p
+
+ join Types_of_Cases t
+
+on p.idPatient =t.idPatient
+
+left join Ward w
+
+on w.idWard =t.idWard
+
+ left join Room_Details rd
+
+on rd.idWard =w.idWard
+
+ join Bed_Details bd
+
+on bd.idBed_Details =rd.Room_No
+
+where t.[discharge date] is NULL
+order by idPatient
+
+
+--order by p.idPatient;
+
+
+select t.idPatient, p.Patient_FName, p.Patient_LName, Case_ID, t.[Admit Date],
+            t.idBed_Details, t.idWard , w.[Floor Number], w.Hospital_ID
+from Types_of_Cases t
+            inner join Patient p on p.idPatient = t.idPatient
+            join Ward w on w.idWard = t.idWard
+
+where [Discharge Date] is null
+order by idPatient;
